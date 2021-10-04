29c9bda96274021b7684f82c3a201b800c9b97d0
pierre <pierre@192.168.115.107>
2021-10-04 15:18:49 +0200

    Fin TP1_manipNombreInt

--- TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
+++ TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
@@ -1,8 +1,12 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TP1 Syntaxe de Base - manipNombreInt
+ * TORDO
+ * BERTOLIN
+ * 4/10/2021
  */
 package tp1_manipnombresint;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +18,19 @@ public class TP1_manipNombresInt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer un premier nombre :");
+        int nb1 = sc.nextInt();
+        
+        System.out.println("\n Entrer un deuxième nombre :");
+        int nb2 = sc.nextInt();
+        System.out.println("Les nombres choisi sont : " + nb1 + " et " + nb2);
+        
+        System.out.println("La somme des 2 nombres est: " + (nb1+nb2));
+        System.out.println("La différence des 2 nombres est: " + (nb1-nb2));
+        System.out.println("La multiplication des 2 nombres est: " + (nb1*nb2));
+        System.out.println("La différence des 2 nombres est: " + (nb1/nb2));
+        System.out.println("Le reste de la division des 2 nombres est: " + (nb1%nb2));
     }
     
 }
