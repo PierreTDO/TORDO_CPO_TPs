738df77b04c0135f2701f7598e53b55c31df5e54
pierre <pierre@192.168.115.107>
2021-10-08 15:57:22 +0200

    Fin_guess_my_Number

--- TP1_guessMyNumber/src/tp1_guessmynumber/TP1_guessMyNumber.java
+++ TP1_guessMyNumber/src/tp1_guessmynumber/TP1_guessMyNumber.java
@@ -1,5 +1,5 @@
 /*
- * TP1 Syntaxe de Base - convertisseur
+ * TP1 Syntaxe de Base - Guess My Number
  * TORDO
  * BERTOLIN
  * 4/10/2021
@@ -19,29 +19,67 @@ public class TP1_guessMyNumber {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Random generateurAleat = new Random();
-        int n = generateurAleat.nextInt(100);
-        System.out.println(n);
+        /*int n2=generateurAleat.nextInt(100);
+        int n3=generateurAleat.nextInt(100);
+        int n4=generateurAleat.nextInt(100);
+        int n5=generateurAleat.nextInt(100);
+        System.out.println(alea + " " + n2 + " " + n3 + " " + n4 + " " + n5 );*/
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n Essayez de deviner le nombre compris entre 0 et 100 :");
-        int nb = sc.nextInt();
-        int i = 1;
-        while (nb != n) {
+        Random generateurAleat = new Random();
 
-            if (nb < n) {
-                System.out.println("\n Essaie encore, valeur trop petite !");
-                nb = sc.nextInt();
-            }
-            if (nb > n) {
-                System.out.println("\n Essaie encore, valeur trop grande !");
-                nb = sc.nextInt();
-
-            }
-            i++;
+        System.out.println("Choisir la difficulté");
+        System.out.println("1) Facile");
+        System.out.println("2) Normal");
+        System.out.println("3) Difficile");
+        int diff = sc.nextInt();
+        while (diff > 3 || diff < 1) {
+            System.out.println("ERREUR La valeur n'est pas comprise entre 1 et 3");
+            System.out.println("Choisir la difficulté");
+            diff = sc.nextInt();
         }
-        if (nb == n) {
-            System.out.println("\n C'est gagné ! en " + i + " coups");
+        int x = 100;
+        int max = 100;
+        switch (diff) {
+            case 1 -> {
+                System.out.println("tu as 100 coups pour trouver le nombre");
+            }
+            case 2 -> {
+                max = 5;
+                System.out.println("tu as 5 coups pour trouver le nombre");
+            }
+            case 3 -> {
+                x = 10000;
+                max = 20;
+                System.out.println("tu as 20 coups pour trouver le nombre");
 
+            }
+        }
+
+        int alea = generateurAleat.nextInt(x);
+
+        System.out.println("Entre 0 et " + x + " : ");
+        int nb = sc.nextInt();
+
+        int z = 1;
+        do {
+            if (max == 0) {
+                System.out.println("Tu n'as plus de coup");
+                break;
+            }
+            if (nb < alea) {
+                System.out.println("trop petit");
+                nb = sc.nextInt();
+            }
+            if (nb > alea) {
+                System.out.println("trop grand");
+                nb = sc.nextInt();
+            }
+            z++;
+            max -= 1;
+        } while (nb != alea);
+        if (nb == alea) {
+            System.out.println(
+                    "exact en " + z + " coups");
         }
     }
 
