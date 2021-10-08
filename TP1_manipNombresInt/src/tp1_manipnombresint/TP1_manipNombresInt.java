5aee8b2d6ece187d544f920f8a9501da093ab094
pierre <pierre@192.168.115.107>
2021-10-08 16:23:44 +0200

    Ajout de commentaire

--- TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
+++ TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
@@ -20,13 +20,13 @@ public class TP1_manipNombresInt {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("\n Entrer un premier nombre :");
-        int nb1 = sc.nextInt();
+        int nb1 = sc.nextInt(); // on demande à l'utilisateur de saisir un permier nombre
         
         System.out.println("\n Entrer un deuxième nombre :");
-        int nb2 = sc.nextInt();
-        System.out.println("Les nombres choisi sont : " + nb1 + " et " + nb2);
+        int nb2 = sc.nextInt(); // on demande à l'utilisateur de saisir un permier nombre
+        System.out.println("Les nombres choisi sont : " + nb1 + " et " + nb2); // on affiche les nombres choisi par l'utilisateur
         
-        System.out.println("La somme des 2 nombres est: " + (nb1+nb2));
+        System.out.println("La somme des 2 nombres est: " + (nb1+nb2)); // on effectue les calculs demandé
         System.out.println("La différence des 2 nombres est: " + (nb1-nb2));
         System.out.println("La multiplication des 2 nombres est: " + (nb1*nb2));
         System.out.println("La différence des 2 nombres est: " + (nb1/nb2));
